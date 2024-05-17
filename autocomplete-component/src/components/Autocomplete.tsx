4dd8d7239f9fc6c1440927fce4a2eeb476c19918
andoniflores <andonibalsebre@gmail.com>
2024-05-16 20:34:30 -0400

    search bar now filters song list with search term

--- autocomplete-component/src/components/Autocomplete.tsx
+++ autocomplete-component/src/components/Autocomplete.tsx
@@ -3,18 +3,30 @@ import { useEffect, useState } from "react";
 
 export default function Autocomplete({ data }: { data: Song[] }) {
   const [showSongList, setShowSongList] = useState<Display>("none");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [filteredSongs, setFilteredSongs] = useState<Song[] | []>([]);
   const divDisplay = {
     display: showSongList,
   };
   useEffect(() => {
-    console.log(data);
-  }, []);
+    filterSongs();
+  }, [searchTerm]);
 
   function searchBarFocus() {
     setShowSongList("block");
   }
   function searchBarBlur() {
     setShowSongList("none");
+  }
+  function filterSongs() {
+    let filterSongs = [];
+    filterSongs = data.filter((song) =>
+      song.songname.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setFilteredSongs(filterSongs);
+  }
+  function handleKeyUp(e: any) {
+    setSearchTerm(e.target.value);
   }
 
   return (
@@ -25,11 +37,12 @@ export default function Autocomplete({ data }: { data: Song[] }) {
           type="search"
           onFocus={searchBarFocus}
           onBlur={searchBarBlur}
+          onKeyUp={handleKeyUp}
         />
       </div>
       <div id="songlist" style={divDisplay}>
         <ul>
-          {data.map((song) => {
+          {filteredSongs.map((song) => {
             return <li>{song.songname}</li>;
           })}
         </ul>
