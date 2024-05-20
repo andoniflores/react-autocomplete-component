437489a8affe6497f14b17203b04932079f24887
andoniflores <andonibalsebre@gmail.com>
2024-05-20 14:34:56 -0400

    changed env variables identifiers to ensure context is clear

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -15,8 +15,8 @@ async function getAccessTokens() {
       },
       body: new URLSearchParams({
         grant_type: "client_credentials",
-        client_id: import.meta.env.VITE_CLIENT_ID,
-        client_secret: import.meta.env.VITE_CLIENT_SECRET,
+        client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+        client_secret: import.meta.env.VITE_SPOTIFY_CLIENT_SECRET,
       }),
     },
   );
